0427ead5270b4c457bdcf078aecfb689f8c846a4
MalekAbdelal <m.abdelal@oun.qa>
2023-10-01 15:54:33 +0300

    Handling the creation of a Library Membership with an overlapping period

--- library_managment/library_managment/doctype/library_member/library_member.py
+++ library_managment/library_managment/doctype/library_member/library_member.py
@@ -8,3 +8,4 @@ class LibraryMember(Document):
 
 	def before_save(self):
 		self.full_name = f"{self.first_name} {self.last_name or ''}"
+
--- library_managment/library_managment/doctype/library_membership/library_membership.py
+++ library_managment/library_managment/doctype/library_membership/library_membership.py
@@ -1,8 +1,18 @@
 # Copyright (c) 2023, Malek and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.model.docstatus import DocStatus
 
 class LibraryMembership(Document):
-	pass
+	
+	def before_submit(self):
+		exists = frappe.db.exists("Library Membership", {
+			"library_member": self.library_member,
+			"docstatus": DocStatus.submitted(),
+			"to_date": (">", self.from_date),
+		},)
+
+		if exists:
+			frappe.throw("There is an active membership for this member")
