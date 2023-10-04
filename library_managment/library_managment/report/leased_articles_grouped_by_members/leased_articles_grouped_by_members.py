94fcf744e85ee4655480a916847f9c4e8c33d1ec
MalekAbdelal <m.abdelal@oun.qa>
2023-10-04 10:57:39 +0300

    Adding script to leased articles grouped by members

--- library_managment/library_managment/report/leased_articles_grouped_by_members/leased_articles_grouped_by_members.py
+++ library_managment/library_managment/report/leased_articles_grouped_by_members/leased_articles_grouped_by_members.py
@@ -1,9 +1,39 @@
 # Copyright (c) 2023, Malek and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 
 
 def execute(filters=None):
-	columns, data = [], []
-	return columns, data
+    columns = [
+        {"label": "Library Member", "fieldname": "library_member", "fieldtype": "Data"},
+        {"label": "Article", "fieldname": "article_name", "fieldtype": "Data"},
+        # Add other columns as needed
+    ]
+
+    data = []
+
+    # Fetch leased articles with member names
+    articles = frappe.db.sql("""
+        SELECT
+            LT.library_member,
+            A.article_name
+        FROM
+            `tabLibrary Transaction` LT
+        INNER JOIN
+            `tabArticle` A
+        ON
+            LT.article_id = A.name 
+        WHERE
+            A.status = 'Issued'
+        """, as_dict=True)
+
+    for article in articles:
+        data.append({
+            "library_member": article["library_member"],
+            "article_name": article["article_name"],
+            # Add other data as needed
+        })
+
+    return columns, data
+
