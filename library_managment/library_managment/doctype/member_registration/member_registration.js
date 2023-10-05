dd1d41089b4b14e395bae5fcff645423e161ae1b
MalekAbdelal <m.abdelal@oun.qa>
2023-10-06 00:15:55 +0300

    Adding script to create member record in Library member doctype for approved Member Registration doctype records

--- library_managment/library_managment/doctype/member_registration/member_registration.js
+++ library_managment/library_managment/doctype/member_registration/member_registration.js
@@ -8,8 +8,20 @@ frappe.ui.form.on('Member Registration', {
 		if (isLibrarian) {
 			frm.add_custom_button('Approve Membership', () => {
 				frm.set_value('status', 'Approved');
-				frm.save();
-			});
+				frm.save().then(() => {
+                    frappe.call({
+                        method: 'member_registration.member_registration.create_library_membership',
+                        args: {
+                            member_registration: frm.doc.name
+                        },
+                        callback: function(response) {
+                            if (!response.exc) {
+                                frappe.msgprint('Library Member added successfully.');
+                            }
+                        }
+                    });
+                });
+            });
 			
 			frm.add_custom_button('Reject Membership', () => {
 				frm.set_value('status', 'Rejected');
--- library_managment/library_managment/doctype/member_registration/member_registration.py
+++ library_managment/library_managment/doctype/member_registration/member_registration.py
@@ -1,8 +1,24 @@
 # Copyright (c) 2023, Malek and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class MemberRegistration(Document):
-	pass
+	pass	
+
+
+@frappe.whitelist()
+def create_library_membership(member_registration):
+    member_registration_doc = frappe.get_doc('Member Registration', member_registration)
+
+    library_membership_doc = frappe.new_doc('Library Membership')
+    library_membership_doc.library_member = member_registration_doc.member_name
+    library_membership_doc.first_name = member_registration_doc.first_name
+    library_membership_doc.last_name = member_registration_doc.last_name
+    library_membership_doc.email_address = member_registration_doc.email_address
+    library_membership_doc.phone = member_registration_doc.phone
+
+    library_membership_doc.insert(ignore_permissions=True)
+
+    return library_membership_doc
