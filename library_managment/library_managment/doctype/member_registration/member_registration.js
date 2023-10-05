a4d8e8ed12e0f27403f3ea8be5f1674532efe0a5
MalekAbdelal <m.abdelal@oun.qa>
2023-10-06 00:02:21 +0300

    Adding script for approve and reject buttons in Member Registration forms

--- library_managment/library_managment/doctype/member_registration/member_registration.js
+++ library_managment/library_managment/doctype/member_registration/member_registration.js
@@ -2,7 +2,18 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Member Registration', {
-	// refresh: function(frm) {
+	refresh: function(frm) {
+		const isLibrarian = frappe.user.has_role('Librarian');
 
-	// }
-});
+		if (isLibrarian) {
+			frm.add_custom_button('Approve Membership', () => {
+				frm.set_value('status', 'Approved');
+				frm.save();
+			});
+			
+			frm.add_custom_button('Reject Membership', () => {
+				frm.set_value('status', 'Rejected');
+				frm.save();
+			});
+		}}
+	})
