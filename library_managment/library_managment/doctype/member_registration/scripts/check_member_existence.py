313016c540cc6ca358aeac1fe246fa6c19f2a784
MalekAbdelal <m.abdelal@oun.qa>
2023-10-06 18:22:28 +0300

    ..

--- library_managment/library_managment/doctype/member_registration/scripts/check_member_existence.py
+++ library_managment/library_managment/doctype/member_registration/scripts/check_member_existence.py
@@ -4,8 +4,8 @@ def check_member_existence(first_name, last_name, email_address):
         exists = frappe.db.exists(
             "Library Member",
             {
-                "first_name": first_name or first_name.lower() or first_name.upper() or first_name.capitalize(),
-                "last_name": last_name or last_name.lower() or last_name.upper() or last_name.capitalize(),
+                "first_name": first_name or str(first_name).lower() or str(first_name).upper() or str(first_name).capitalize(),
+                "last_name": last_name or str(last_name).lower() or str(last_name).upper() or str(last_name).capitalize(),
                 "email_address": email_address,
             },
         )
