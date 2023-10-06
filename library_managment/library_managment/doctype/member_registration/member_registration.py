0e22b31ff49cab52304206804dc07c4ac117d17e
MalekAbdelal <m.abdelal@oun.qa>
2023-10-06 17:55:52 +0300

    check if the person is already a member in the library for lirary registration

--- library_managment/library_managment/doctype/member_registration/member_registration.py
+++ library_managment/library_managment/doctype/member_registration/member_registration.py
@@ -3,7 +3,12 @@
 
 import frappe
 from frappe.model.document import Document
+from scripts.check_member_existence import check_member_existence
 
 class MemberRegistration(Document):
-	pass	
+	
+    def before_submit(self):
+
+        if check_member_existence(self.first_name, self.last_name, self.email_address):
+            frappe.throw("This person is already a member in the library")
 
