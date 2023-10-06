7b0b078559e23366a092ebbcee93f597ea4cc85a
MalekAbdelal <m.abdelal@oun.qa>
2023-10-06 19:54:22 +0300

    ..

--- library_managment/library_managment/doctype/member_registration/scripts/filter_current_user_requests.py
+++
@@ -1,6 +0,0 @@
-# import frappe
-
-# def get_filtered_records(doctype, txt, filters, limit_start, limit_page_length, order_by, sort_order, user):
-#     filtered_records = frappe.get_all(doctype, filters={'owner': user}, fields=['first_name', 'last_name', 'email_address', 'phone', 'status'], limit_start=limit_start, limit_page_length=limit_page_length, order_by=order_by, sort_order=sort_order)
-
-#     return filtered_records
